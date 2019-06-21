bf5c8f64091219560e48fa0f3d1926cbc267aca1
Christoph <ckluente@thoughtworks.com>
2019-06-21 19:06:15 +0200

    stubs gocd
    much faster :)

--- integration-test/test-integration.sh
+++ integration-test/test-integration.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
 
-npm install
 docker-compose up -d
-while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' localhost:8153/go/api/v1/health)" != "200" ]]; do 
-  echo -n '.'
-  sleep 5 
-done
-echo 'GoCD is up'
-
-sleep 30 # gocd is up, but needs some time to discover its pipelines
+npm install
+sleep 10 # wait until mountebank is ready
 npm test
