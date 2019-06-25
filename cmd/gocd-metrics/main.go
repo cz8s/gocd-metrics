02eccf3e70c5d4e662c6e2376a5d004fd329e96c
Andrew Bentley <andrew@bentley.codes>
2019-06-25 22:21:45 +0200

    Revert "uses cobra for arg parsing"
    This reverts commit a3a8515da5b6d37e06c1b4d42ba5480570a7222b.

--- cmd/gocd-metrics/main.go
+++ cmd/gocd-metrics/main.go
@@ -1,66 +1,70 @@
 package main
 
 import (
+	"flag"
 	"log"
-	"net/http"
 	"os"
 	"time"
+
+	"net/http"
+	_ "net/http/pprof"
 
 	"github.com/cz8s/gocd-metrics/pkg/observe"
 	"github.com/cz8s/gocd-metrics/pkg/web"
 	"github.com/facebookgo/grace/gracehttp"
-	"github.com/spf13/cobra"
 )
 
 var (
-	metricsAddress            string
-	gocdAddress               string
-	gocdUsername              string
-	gocdPassword              string
-	gocdSkipTLSVerify         bool
-	gocdScrapeIntervalSeconds = 10
-	writeTimeout              = time.Second * 30
-	readTimeout               = time.Second * 30
-	rootCmd                   = &cobra.Command{
-		Use:   "gocd-metrics",
-		Short: "Exports GoCD build metrics to prometheus.",
-		Long:  `Exports GoCD build metrics to prometheus.`,
-		Run: func(cmd *cobra.Command, args []string) {
-			r := web.NewRouter()
-			r = observe.RegisterPrometheus(r)
-			srv := &http.Server{
-				Handler:      r,
-				Addr:         metricsAddress,
-				WriteTimeout: writeTimeout,
-				ReadTimeout:  readTimeout,
-			}
-			gocdClient := observe.NewGocdClient(gocdAddress, gocdUsername, gocdPassword, gocdSkipTLSVerify)
-			metrics := observe.NewGocdMetrics()
+	addr              = flag.String("addr", "0.0.0.0:9090", "Primary HTTP addr")
+	debug             = flag.Bool("debug", false, "Set the logging level to debug")
+	gocdHost          = flag.String("gocd-host", env("GOCD_HOST"), "The host for the GoCD server, including scheme and port.")
+	gocdUsername      = flag.String("gocd-username", env("GOCD_USERNAME"), "The username for the GoCD user.")
+	gocdPassword      = flag.String("gocd-password", env("GOCD_PASSWORD"), "The password for the GoCD user.")
+	gocdSkipTLSVerify = flag.Bool("gocd-skip-tls-verify", envBool("GOCD_SKIP_TLS_VERIFY"), "Set to true to not verify the authenticity the GoCD server's TLS certificate.")
 
-			go func() {
-				for {
-					if err := observe.UpdateGocdMetrics(&metrics, gocdClient); err != nil {
-						log.Print(err)
-					}
-					observe.UpdatePrometheus(metrics)
-					time.Sleep(time.Duration(time.Second * time.Duration(gocdScrapeIntervalSeconds)))
-				}
-			}()
-
-			gracehttp.Serve(srv)
-		},
-	}
+	writeTimeout = time.Second * 30
+	readTimeout  = time.Second * 30
 )
 
 func main() {
-	rootCmd.Flags().StringVar(&metricsAddress, "addr", "0.0.0.0:9090", "The address for exposing the metrics server")
-	rootCmd.Flags().StringVar(&gocdAddress, "gocd-url", "", "The address for the GoCD server, including scheme and port")
-	rootCmd.MarkFlagRequired("gocd-url")
-	rootCmd.Flags().StringVar(&gocdUsername, "gocd-username", "", "The username for the GoCD user")
-	rootCmd.Flags().StringVar(&gocdPassword, "gocd-password", "", "The password for the GoCD user")
-	rootCmd.Flags().BoolVar(&gocdSkipTLSVerify, "gocd-skip-tls-verify", false, "Skip verification of the authenticity of the GoCD server's TLS certificate")
+	flag.Parse()
 
-	if err := rootCmd.Execute(); err != nil {
-		os.Exit(1)
+	r := web.NewRouter()
+	r = observe.RegisterPrometheus(r)
+	srv := &http.Server{
+		Handler:      r,
+		Addr:         *addr,
+		WriteTimeout: writeTimeout,
+		ReadTimeout:  readTimeout,
 	}
+	gocdClient := observe.NewGocdClient(*gocdHost, *gocdUsername, *gocdPassword, *gocdSkipTLSVerify)
+	metrics := observe.NewGocdMetrics()
+
+	go func() {
+		for {
+			if err := observe.UpdateGocdMetrics(&metrics, gocdClient); err != nil {
+				log.Print(err)
+			}
+			observe.UpdatePrometheus(metrics)
+			time.Sleep(time.Duration(10 * time.Second))
+		}
+	}()
+
+	gracehttp.Serve(srv)
+}
+
+func env(name string) string {
+	value, ok := os.LookupEnv(name)
+	if !ok {
+		return ""
+	}
+	return value
+}
+
+func envBool(name string) bool {
+	value, ok := os.LookupEnv(name)
+	if !ok {
+		return false
+	}
+	return value == "true"
 }
